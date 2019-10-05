de4ae7762dfee0c642ff5aaa7b73687429fe1c97
Gurbx <philip.lindberg101@gmail.com>
2019-10-05 10:24:16 +0200

    Setup and basic assets

---
+++ LD45/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+
+    private Rigidbody2D rigidbody;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();        
+    }
+
+    void Update()
+    {
+        
+    }
+}
